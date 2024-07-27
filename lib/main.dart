6ff1528267e2dbc0a79d0d1b3c417957a6802dab
Maite <maicapref@gmail.com>
2024-07-27 20:55:09 -0300

    password change function done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,19 +13,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return SharedDataContainer(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => MyHomePage(),
+          '/login': (context) => LoginScreen(),
+          '/change': (context) => ChangePasswordScreen(),
+        },
       ),
-    initialRoute: '/',
-    routes: {
-      '/': (context) => MyHomePage(),
-      '/login': (context) => LoginScreen(),
- //     '/data': (context) = DataScreen(email: email, senha: senha),
-      '/change': (context) => ChangePasswordScreen()
-    }
     );
   }
 }
@@ -77,14 +79,76 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class LoginScreen extends StatelessWidget {
+class SharedDataContainer extends StatefulWidget {
+  final Widget child;
 
+  const SharedDataContainer({Key? key, required this.child}) : super(key: key);
+
+  @override
+  _SharedDataContainerState createState() => _SharedDataContainerState();
+
+  static _SharedDataContainerState? of(BuildContext context) {
+    return context.findAncestorStateOfType<_SharedDataContainerState>();
+  }
+}
+
+class _SharedDataContainerState extends State<SharedDataContainer> {
+  String? email;
+  String? senha;
+
+  void updateData(String newEmail, String newSenha) {
+    setState(() {
+      email = newEmail;
+      senha = newSenha;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SharedData(
+      email: email,
+      senha: senha,
+      child: widget.child,
+    );
+  }
+}
+
+
+class SharedData extends InheritedWidget {
+  final String? email;
+  final String? senha;
+
+  const SharedData({
+    Key? key,
+    required Widget child,
+    this.email,
+    this.senha,
+  }) : super(key: key, child: child);
+
+  static SharedData? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<SharedData>();
+  }
+
+  @override
+  bool updateShouldNotify(covariant SharedData oldWidget) {
+    return oldWidget.email != email || oldWidget.senha != senha;
+  }
+}
+
+
+
+class LoginScreen extends StatefulWidget {
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
 //  const LoginScreen({super.key});
   final emailController = TextEditingController();
+
   final senhaController = TextEditingController();
-//  String? email;
-//  String? senha;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -99,14 +163,12 @@ class LoginScreen extends StatelessWidget {
             children: <Widget>[
               TextFormField(
                 controller: emailController,
-//                onChanged: (novoValor) => email = novoValor,
                 decoration: InputDecoration(
                   hintText: 'Informe seu e-mail'
                 ),
               ),
               TextFormField(
                 controller: senhaController,
-//                onChanged: (novoValor) => senha = novoValor,
                 obscureText: true,
                 decoration: InputDecoration(
                   hintText: 'Informe sua senha'
@@ -117,8 +179,13 @@ class LoginScreen extends StatelessWidget {
                 children: <Widget>[
                   TextButton(
                     child: Text('Logar'),
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: ((context) => DataScreen(email: emailController.text, senha: senhaController.text))));
+                    onPressed: () {
+                      SharedDataContainer.of(context)?.updateData(
+                        emailController.text,
+                        senhaController.text,);
+                        
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => DataScreen()));
+                        //builder: ((context) => DataScreen(email: emailController.text, senha: senhaController.text))));
                     },
                   )
                 ],
@@ -132,13 +199,19 @@ class LoginScreen extends StatelessWidget {
 }
 
 class DataScreen extends StatelessWidget {
-  const DataScreen({required this.email, required this.senha});
-
-  final String email;
-  final String senha;
-
+  //const DataScreen({required this.email, required this.senha});
+  //final String email;
+  //final String senha;
+  
   @override
   Widget build(BuildContext context) {
+
+    final sharedData = SharedData.of(context);
+    print('email');
+    print(sharedData?.email);
+    print('senha');
+    print(sharedData?.senha);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('E-mail e senha'),
@@ -146,8 +219,8 @@ class DataScreen extends StatelessWidget {
       body: Container(
         child: Column(
           children: <Widget>[
-            Text(email),
-            Text(senha)
+            Text(sharedData?.email ?? 'E-mail não disponível'),
+            Text(sharedData?.senha ?? 'Senha não disponível')
           ],
         ),
       ),
@@ -155,12 +228,23 @@ class DataScreen extends StatelessWidget {
   }
 }
 
-class ChangePasswordScreen extends StatelessWidget {
+class ChangePasswordScreen extends StatefulWidget {
+
+  @override
+  State<ChangePasswordScreen> createState() => _ChangePasswordScreenState();
+}
+
+class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   final novaSenhaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+
+    final sharedData = SharedData.of(context);
+    print(sharedData?.senha);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Mudar senha'),
@@ -168,18 +252,44 @@ class ChangePasswordScreen extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.all(15.0),
         child: Form (
+          key: _formKey,
           child: Column(
             children: <Widget>[
               TextFormField(
                 decoration: InputDecoration(
                   hintText: 'Informe senha atual'
-                )
+                ),
+                validator: (String? value) {
+                  if(value != sharedData?.senha) {
+                    print(value);
+                    print(sharedData?.senha);
+                    return 'Senha incompatível';
+                  }
+                },
               ),
+
               TextFormField(
                 controller: novaSenhaController,
                 decoration: InputDecoration(
                   hintText: 'Informe nova senha'
                 ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    child: Text('Alterar'),
+                    onPressed: () {
+                       if (_formKey.currentState!.validate()) {
+                          SharedDataContainer.of(context)?.updateData(
+                            SharedDataContainer.of(context)?.email ?? '',
+                            novaSenhaController.text,
+                          );
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => DataScreen()));
+                      }
+                    },
+                  )
+                ],
               )
             ],
           ),
@@ -187,5 +297,4 @@ class ChangePasswordScreen extends StatelessWidget {
       ),
     );
   }
-
 }
